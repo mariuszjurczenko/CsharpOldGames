c269aee895338e401360593c66cd550a87f52970
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-01-28 09:53:01 +0100

    Krok 1 - Wyświetlimy planszę (Canvas) oraz tworzymy animację opadającego bloku, co pozwoli nam przetestować naszą logikę w praktyce - MainWindow

--- Tetris/src/Tetris.Presentation/App.xaml.cs
+++ Tetris/src/Tetris.Presentation/App.xaml.cs
@@ -1,14 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace Tetris.Presentation
+﻿namespace Tetris.Presentation
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
     }
-
 }
--- Tetris/src/Tetris.Presentation/MainWindow.xaml.cs
+++ Tetris/src/Tetris.Presentation/MainWindow.xaml.cs
@@ -1,24 +1,78 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Tetris.Application.Services;
+using Tetris.Domain.Models;
 
-namespace Tetris.Presentation
+namespace Tetris.Presentation;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private readonly GameService _gameService;
+    private readonly DispatcherTimer _gameTimer;
+
+    private const int BlockSize = 20;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        var gameBoard = new GameBoard(10, 20);
+        _gameService = new GameService(gameBoard);
+
+        _gameTimer = new DispatcherTimer
         {
-            InitializeComponent();
+            Interval = TimeSpan.FromMilliseconds(500)
+        };
+
+        _gameTimer.Tick += GameTick;
+        _gameService.SpawnBlock();
+        _gameTimer.Start();
+    }
+
+    private void GameTick(object? sender, EventArgs e)
+    {
+        _gameService.MoveBlockDown();
+        Render();
+    }
+
+    private void Render()
+    {
+        GameCanvas.Children.Clear();
+
+        foreach (var block in _gameService.GetBlocks())
+        {
+            var rect = new Rectangle
+            {
+                Width = BlockSize,
+                Height = BlockSize,
+                Fill = new SolidColorBrush(GetColor(block.Color)),
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 1
+            };
+
+            // Ustawienie pozycji bloku na Canvas
+            Canvas.SetLeft(rect, block.X * BlockSize);
+            Canvas.SetTop(rect, block.Y * BlockSize);
+
+            GameCanvas.Children.Add(rect);
         }
+    }
+
+    private Color GetColor(ConsoleColor color)
+    {
+        return color switch
+        {
+            ConsoleColor.Cyan => Colors.Cyan,
+            ConsoleColor.Yellow => Colors.Yellow,
+            ConsoleColor.Blue => Colors.Blue,
+            ConsoleColor.DarkBlue => Colors.DarkBlue,
+            ConsoleColor.Magenta => Colors.Magenta,
+            ConsoleColor.Green => Colors.Green,
+            ConsoleColor.Red => Colors.Red,
+            _ => Colors.White,
+        };
     }
 }
