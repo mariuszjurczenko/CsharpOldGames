0d2dc274d0a539be67c5caaa538b6c1a8341f5aa
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-01-28 09:40:16 +0100

    Krok 1 - stworzenie modeli odpowiadających za podstawowe elementy gry: Block, GameBoard

---
+++ Tetris/src/Tetris.Domain/Models/Block.cs
@@ -0,0 +1,8 @@
+﻿namespace Tetris.Domain.Models;
+
+public class Block
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public ConsoleColor Color { get; set; }
+}
---
+++ Tetris/src/Tetris.Domain/Models/GameBoard.cs
@@ -0,0 +1,26 @@
+﻿namespace Tetris.Domain.Models;
+
+public class GameBoard
+{
+    public int Width { get; }
+
+    public int Height { get; }
+
+    public Block[,] Grid { get; }
+
+
+    public GameBoard(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Grid = new Block[height, width];
+    }
+
+    public bool IsCellEmpty(int x, int y) => Grid[y, x] == null;
+
+    public void PlaceBlock(Block block)
+    {
+        if (block.Y >= 0 && block.Y < Height && block.X >= 0 && block.X < Width)
+            Grid[block.Y, block.X] = block;
+    }
+}
